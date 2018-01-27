f5c6cab968c59ede8691ce4a83d186f4c8430983
PeterChen <747853229@qq.com>
2018-01-27 10:12:26 +0800

    Fix the img displays method

--- tab.js
+++ tab.js
@@ -1,20 +1,28 @@
 const bgImg = document.querySelector('.bg-img')
 const currentTime = document.querySelector('#currentTime')
-
+let r = 0
 
 function updateTime() {
     let time = new Date()
     let minutes = time.getMinutes() + ''
+    let hours = time.getHours() + ''
     if(minutes.length == 1) {
         minutes = '0' + minutes
+    } else if(hours.length == 1) {
+        hours = '0' + hours
     }
-    currentTime.textContent = time.getHours() + ' : ' + minutes
+    currentTime.textContent = hours + ':' + minutes
 }
 
+function fadeIn(img) {
+    img.classList.add('fadein')
+}
 
+r = Math.floor(10 * Math.random())  
 
-bgImg.style.backgroundImage = 'url("https://bing.ioliu.cn/v1?d=0&w=1920&h=1080")'
-
+// bgImg.style.backgroundImage = `url("https://bing.ioliu.cn/v1?d=${r}&w=1920&h=1080")`
+bgImg.style.backgroundImage = `url("./imgs/bg1.png")`
+bgImg.onLoad = fadeIn(bgImg)
 
 
 updateTime()
